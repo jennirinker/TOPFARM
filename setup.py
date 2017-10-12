2b190218e8d8e4b2ad3ab33a8a59ea82104acfe3
Jennifer Rinker <rink@win.dtu.dk>
2017-10-12 14:26:41 +0200

    initial adding setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# from distutils.core import setup
+from setuptools import setup
+
+
+setup(
+    name='topfarm',
+    version='0.1dev',
+    description='',
+    author='TOPFARM Developers',
+    author_email='',
+    packages=['topfarm'],
+)
+
